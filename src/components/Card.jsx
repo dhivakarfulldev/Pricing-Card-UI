4d43bf1f215c4d72a98a87ceb6d84ee892035328
dhivakarfulldev <dhivakarfulldev@gmail.com>
2026-08-12 19:55:09 +0530

    updated files

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
 
 
 const Card = ({plan}) => {
+  
   return (
     <div className="card">
      <h4 className="card-title">{plan.title}</h4>
      <h3 className="card-price">{plan.price}</h3>
      <ul>
         {
-            plan.features.map((feature) => (<li><i className="fa-solid fa-check icon"></i>  {feature.text}</li>))
+            plan.features.map((feature) => (<li className={feature.available ? "icon-check":"icon-wrong"}><i className={`fa-solid ${feature.available ? "fa-check " :"fa-x"} icon`}></i>  {feature.text}</li>))
         }
         
      </ul>
